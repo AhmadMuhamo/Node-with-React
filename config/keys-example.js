9c9e54cf581c0c038a1c1a0a59a927e35cfeab9a
Ahmad Ibrahim <ahmadibrahim@Ahmads-MacBook-Pro.local>
2018-12-15 18:30:53 +0200

    Added Google OAuth2.0

---
+++ config/keys-example.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID: 'CLIENT_ID',
+    googleClientSecret: 'CLIENT_SECRET'
+};
+
+// 1- Replace CLIENT_ID and CLIENT_SECRET with google's Client ID and Secret
+// 1- Rename the file to keys.js
