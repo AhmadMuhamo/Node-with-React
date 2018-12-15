5c094d71b2c8874603473c3f5c01e67774efb771
Ahmad Ibrahim <ahmadibrahim@Ahmads-MacBook-Pro.local>
2018-12-15 18:43:13 +0200

    reconfigured prettier to use single quotes

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
 passport.use(
-  new GoogleStrategy(
-    {
+  new GoogleStrategy({
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
       console.log(accessToken);
@@ -21,12 +20,12 @@ passport.use(
 );
 
 app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
   })
 );
 
-app.get("/auth/google/callback", passport.authenticate("google"));
+app.get('/auth/google/callback', passport.authenticate('google'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
