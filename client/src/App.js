41d10010959d5d11bbba8a624765e3add8206ef5
Ahmad Ibrahim <ahmadmuhamo@gmail.com>
2018-12-17 14:38:39 +0200

    react proxy issue fix

--- client/src/App.js
+++ client/src/App.js
@@ -8,16 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+          <p>Hello</p>
+          <a className="App-link" href="/auth/google" rel="noopener noreferrer">
+            Click to sign in with google!
           </a>
         </header>
       </div>
