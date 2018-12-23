0c37833b2806f9ef7ac02419260270f2aaec8dd7
Ahmad Ibrahim <ahmadmuhamo@gmail.com>
2018-12-23 16:16:02 +0200

    added materialize-css

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
-import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import 'materialize-css/dist/css/materialize.min.css';
 
 const store = createStore(reducers, {}, applyMiddleware());
 
